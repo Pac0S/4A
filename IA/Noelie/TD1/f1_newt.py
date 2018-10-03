7ea398ca17d397d5f4a6c641b01924210ccd1c1e
Pac0o0 <paco.salomon@gmail.com>
2018-10-03 09:58:39 +0200

    Newton propre

--- IA/Noelie/TD1/f1_newt.py
+++ IA/Noelie/TD1/f1_newt.py
@@ -5,6 +5,7 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import opti1 as op
 #--------------------------------------
 
 #Definition of what to plot
@@ -17,64 +18,42 @@ Z= (X-Y)**4 + 2*X**2 + Y**2 - X + 2*Y #defines the function values
 surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, linewidth=0, antialiased=False, cmap=cm.viridis) #plot definition and options
 
 
-
-
-
-def trace_grad(n,x0,y0,a):
+n=2
+x0=1
+y0=1
+def trace_newt(n,x0,y0):
 	z0 = (x0-y0)**4 + 2*x0**2 + y0**2 - x0 + 2*y0
-	
-	x = []
-	y = []
-	z = []
-	
-	x.append(x0)
-	y.append(y0)
-	z.append(z0)
-	
-	xnow = x0
-	ynow = y0
-	znow = z0
-	
-	grad_xnow = 4*(xnow-ynow)**3+4*xnow-1
-	grad_ynow = -4*(xnow-ynow)**3+2*ynow+2
-		
-	k=0
-	
-	#while(math.sqrt(grad_xnow**2+grad_ynow**2) <0.000001):
+	px = x0
+	py = y0
+	pz = z0
+	x = [x0]
+	y = [y0]
+	z = [z0]
 	for k in range(n):
-		#Calcul du gradient
-		grad_xnow = 4*(xnow-ynow)**3+4*xnow-1
-		grad_ynow = -4*(xnow-ynow)**3+2*ynow+2
-		#Calcul de la direction
-		direc_xnow = -grad_xnow
-		direc_ynow = -grad_ynow
-		#Calcul des nouvelles coordonnees
-		xnow = xnow + a * direc_xnow
-		ynow = ynow + a * direc_ynow
-		znow = (xnow-ynow)**4 + 2*xnow**2 + ynow**2 - xnow + 2*ynow
-		#Ajout des nouvelles coordonnees aux listes
-		x.append(xnow)
-		y.append(ynow)
-		z.append(znow)
-		k=k+1
+		p = op.new_position_newt(px,py)
+		px = p[0]
+		py = p[1]
+		pz = (px-py)**4 + 2*px**2 + py**2 - px + 2*py
+		x.append(px)
+		y.append(py)
+		z.append(pz)
 	return x,y,z
-	
-a = 0.01
-n=100
-
+		
 valx0 = [-0.5,0.5,1]
 valy0 = [-0.5,0.5,1]
 col_ind = 0
 colors = ['b', 'g', 'r', 'c', 'm', 'y',  'k', 'w', 'tab:orange', 'slateblue', 'lime', 'maroon', 'gold', 'grey', 'indigo', 'ivory' ]
 for i in valx0:
 	for j in valy0:
-		tr = trace(n,i,j,a)
+		tr = trace_newt(n,i,j)
 		x = tr[0]
 		y = tr[1]
 		z = tr[2]
-		ax.plot(x,y,z,label = "(x0,y0)="+"("+str(i)+","+str(j)+")",color = colors[col_ind])
+		ax.plot(x,y,z,color = colors[col_ind])
 		col_ind= col_ind+1
 	
 #Runs the plot command
 plt.show()
+
+
 
--- IA/Noelie/TD1/opti1.py
+++ IA/Noelie/TD1/opti1.py
@@ -11,17 +11,17 @@ def gradient(x,y):
 def direction_grad(x,y):
 	grad = gradient(x,y)
 	direc = [-grad[0], -grad[1]]
-	print("Direction : " + str(direc[0]) + ", " + str(direc[1]) )
+	#print("Direction : " + str(direc[0]) + ", " + str(direc[1]) )
 	return direc
 	
 def new_position_grad(x,y,alpha):
-	print("Position : " + str(x) + ", " + str(y))
+	#print("Position : " + str(x) + ", " + str(y))
 	d = direction_grad(x,y)
 	new_x = x + alpha * d[0]
 	new_y = y + alpha * d[1]
 	new_pos = [new_x, new_y]
-	print("Nouvelle position : " + str(new_x) + ", " + str(new_y))
-	print('')
+	#print("Nouvelle position : " + str(new_x) + ", " + str(new_y))
+	#print('')
 	return new_pos
 	
 def direction_newt(x,y):
@@ -60,36 +60,27 @@ px = x0
 py = y0
 p = [px,py] #Point actuel
 a = 0.09 #pas de convergence
+lim = 0.000001 #Limite gradient
 
 
 #################################
 #		Descente de gradient	#
 #################################
-'''
-#Condition du nombre d'iterations
-for k in range(100):	
-	print(k)
-	p = new_position(px,py,a)
-	px = p[0]
-	py = p[1]
 
-print("Position finale : "+ str(px) +", " + str(py))
-'''
 
-"""
-#Condition de la norme du gradient
+print("METHODE DE LA DESCENTE DU GRADIENT")
 grad = gradient(px,py)
 k=0
-while (math.sqrt(grad[0]**2+grad[1]**2)>0.000001):
-	p = new_position(px,py,a)
+#for k in range(100):
+while (math.sqrt(grad[0]**2+grad[1]**2)>lim):	
+	p = new_position_grad(px,py,a)
 	grad = gradient(px,py)
 	px = p[0]
 	py = p[1]
 	k= k+1
-	print(k)
+print("Gradient < " + str(lim) + ", iteration no "+str(k))
 print("Position finale : "+ str(px) +", " + str(py))
-
-"""
+print('')
 
 #################################
 #		Methode de Newton		#
@@ -97,11 +88,18 @@ print("Position finale : "+ str(px) +", " + str(py))
 
 
 print('METHODE DE NEWTON')
-for k in range(100):
-	#print(k)
+px = x0
+py = y0
+k=0
+grad = gradient(px,py)
+#for k in range(100):
+while(math.sqrt(grad[0]**2+grad[1]**2)>lim):
 	p = new_position_newt(px,py)
+	grad = gradient(px,py)
 	px = p[0]
 	py = p[1]
+	k=k+1
+print("Gradient < " + str(lim) + ", iteration no "+str(k))
 print("Position finale : "+ str(px) +", " + str(py))
 	
 
