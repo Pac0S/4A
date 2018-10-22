59c1fbf75a13d82e5e09b3bcc810c0d9e2c8393b
Paco <paco.salomon@gmail.com>
2018-10-22 17:34:02 +0200

    Levenberg marquardt marche

--- IA/Noelie/TD2/opti2.py
+++ IA/Noelie/TD2/opti2.py
@@ -9,33 +9,47 @@ import numpy as np
 
 
 
-
+#Fonction g appliquee a une valeur x pour une valeur de a donnee
 def g(x,a):
 	g = math.exp(-a*x)
 	return g
+	
+	
+def derg(x,a):
+	dgda = -x*math.exp(-a*x)
+	return dgda
+	
+		
 
 ## f est la somme des erreurs au carre ##
 def f(x,y,a):
 	f = 0
 	for i in range(len(x)):
 		f = f + (y[i] - g(x[i], a))**2
-	f = f/2
+	f = f*0.5
 	return f
+	
+
+
 
 # Gradient de f en fonction de a #
 def gradient(x,y,a):
 	grad = 0
 	for i in range(len(x)):
-		grad = grad + (y[i] - g(x[i], a))* (-x[i] * math.exp(-a*x[i]))
+		grad = grad + (y[i] - g(x[i], a)) * (-x[i] * math.exp(-a*x[i]))
 	grad = -grad
 	return grad
 	
+
+	
 #Derivee 2nde de f par rapport a une variable a
-def deriveef2(x,a):
+def derf2(x,a):
 	der = 0
 	for i in range(len(x)):
-		der = der + (-x[i]* math.exp(-a*x[i]))**2 #Derivee de g au carre
+		der = der + (derg(x[i],a)**2) #Derivee de g au carre (en fo de a)
 	return der
+	
+	
 	
 
 def lev_mar(l_init, a_init, x, y):
@@ -43,11 +57,11 @@ def lev_mar(l_init, a_init, x, y):
 	a = a_init
 	k = 1
 	grad = 1
-	while (k <= 20) :#and abs(grad) > 0.00001):
+	while (k <= 20 and abs(grad) > 0.00001):
 		print("k = " + str(k))
 		print ("lambda : " + str(l) + "\na : " + str(a))
 		grad = gradient(x,y,a) #Gradient de f en x
-		der2 = deriveef2(x,a) #derivee seconde de la fonction f
+		der2 = derf2(x,a) #derivee seconde de la fonction f
 		#middle = True
 		#while (middle == True):
 		hLM = der2*(1+l)  #Matrice hessienne ponderee = derivee seconde ponderee dans ce cas
@@ -67,54 +81,113 @@ def lev_mar(l_init, a_init, x, y):
 		print("f_next < f_act : ", not middle)
 		print ("direction : " + str(d) + "\n\n")
 		
-		
-		
 	
 	
 
 if __name__=="__main__":
 
 
-	###Test fonction g###
-	#g1 = g(1,1)
-	#print("g = " + str(g1))
+	#################################################
+	#		Test fonction g et derivee seconde		#
+	#################################################
 	
+	###x = 1, a = 1###
 	
-	###Creation du jeu de donnees bruite###
+	g1 = g(1,1)
+	dg1 = derg(1,1)
+	print("a=1, x=1, g = " + str(g1))
+	print("a=1, x=1, dg/da = " + str(dg1))
 	
-	a = 1.5
+	#########################
+	#		Parametres		#
+	#########################
+	
+	a = 2
 	b = 0.01
 	
-	data = np.zeros((2,301))
-	data[0] = np.arange(0,3.01,0.01)
-	gs = np.zeros((301)) #Liste des valeurs theoriques
-	for i in range(len(data[0])):
-		data[1][i] = g(data[0][i],a) + b * np.random.randn()
-		gs[i] = g(data[0][i],a)
 	
-	f1 = f(data[0],data[1],a)
-	print("f = " + str(f1))
+	#####################################################
+	#		Creation du jeu de donnes non bruitees		#
+	#####################################################
 	
-	grad1 = gradient(data[0],data[1],a)
-	print("gradient : " + str(grad1))
+	init = (2,301)
+	nonoised = np.zeros(init)
 	
-	der1 = deriveef2(data[0],a)
-	print("derivee 2nde : " + str(der1))
+	
+	xi=0
+	for i in range(len(nonoised[0])):
+		nonoised[0][i] = xi
+		xi+=0.01
+	
+	i=0
+	for xi in nonoised[0]:
+		nonoised[1][i] = g(xi,a)
+		i+=1
+	#print(nonoised)
+	
+	
+	#################################################
+	#		Creation du jeu de donnes bruitees		#
+	#################################################
+
+	
+	noised = np.zeros(init)
+	
+	xi=0
+	for i in range(len(noised[0])):
+		noised[0][i] = xi
+		xi+=0.01
+	
+	i=0
+	for xi in noised[0]:
+		noised[1][i] = g(xi,a)+ b * np.random.randn()
+		i+=1
+	#print(noised)
+	
+	#############################################
+	#		Test de f sur les donnees bruitees	#
+	#############################################
+	
+	f1 = f(noised[0], noised[1], a)
+	print("f donnees bruitees : " + str(f1))
+	#f = 0 sur les donnees non bruitees (somme des ecarts au carre = 0)
+	
+	
+	#####################################################
+	#		Test de gradient sur les donnees bruitees	#
+	#####################################################
+	
+	grad1 = gradient(noised[0], noised[1], a)
+	print("gradient donnees bruitees : " + str(grad1))
+	#grad = 0 sur les donnees non bruitees
+	
+	
+	#####################################################
+	#		Test de derivee seconde pour la fonction g	#
+	#####################################################
+	
+	df2 = derf2(noised[0], a)
+	print("derivee seconde de f par rapport a a : " + str(df2))
 	
 	print("")
 	print("")
 	
-	lev_mar(0.001, 1.5, data[0], data[1])
+	#####################################################
+	#		Test de la methodede Levenberg-Marquardt	#
+	#####################################################	
+
+	lev_mar(0.001, 1.5, noised[0], noised[1])
+	#lev_mar(lambda, a,		x,		y	)
 
 	#print(data)
-	
+	'''
 	plt.plot(data[0], gs, 'r', label = 'Courbe theorique de g')
 	plt.scatter(data[0],data[1], s=1, c='g', marker='o', label = 'Valeurs bruitees de g')
 	plt.legend(loc = 'upper right')
 	plt.xlabel('x')
 	plt.ylabel('y')
-	
-	plt.show()
+	'''
+	#plt.show()
 #Modification inutile
 	
 	
