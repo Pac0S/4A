a01e3160c284151f592284a980633486b896972a
Paco <paco.salomon@gmail.com>
2018-10-22 23:11:57 +0200

    cas 1 opti2 fini

--- IA/Noelie/TD2/opti2.py
+++ IA/Noelie/TD2/opti2.py
@@ -70,41 +70,66 @@ def derf2(x,a):
 	#		Methode de Levenberg-Marquardt	#
 	#########################################	
 
-def lev_mar(l_init, a_init, x, y):
+def lev_mar(l_init, a_init, x, y, n, grad_lim):
 	l = l_init
 	a = a_init
 	k = 1
 	grad = 1
+	lambda_list = []
+	a_list = []
+	f_list =[]
+	grad_list = []
+	k_list = []
 	
-	
-	while (k <= 20 and abs(grad) > 0.00001):
+	while (k <= n):# and abs(grad) > grad_lim):
+		
+		#Ajout de k a une liste
 		print("k = " + str(k))
-		print ("lambda : " + str(l) + "\na : " + str(a))
+		k_list.append(k)
+		
+		#Ajout de lambda a une liste
+		print ("lambda : " + str(l))
+		lambda_list.append(l)
+		
+		#Ajout dea a une liste
+		print( "a : " + str(a))
+		a_list.append(a)
+		
+		#ajout du gradient a une liste
 		grad = gradient(x,y,a) #Gradient de f en x
+		print ("Gradient : " + str(grad))
+		grad_list.append(abs(grad))
+		
 		der2 = derf2(x,a) #derivee seconde de la fonction f
-		#middle = True
-		#while (middle == True):
 		hLM = der2*(1+l)  #Matrice hessienne ponderee = derivee seconde ponderee dans ce cas
 		d = - grad/hLM #Direction de descente
+		
+		#calcul et ajout de f a une liste
 		f_act = f(x,y,a)
 		f_next = f(x,y,a+d)
-		
-		
+		f_list.append(f_act)
+		print("f_act : " + str(f_act) + "\nf_next : " + str(f_next))
+			
+			
+		#Verification de la condition
 		if f_next < f_act :
 			a = a+d
 			l = l/10
-			middle = False
+			#middle = False
 		else :
 			l = l*10
-			middle = True
+			#middle = True
 			
 		
 		k += 1
-		print ("Gradient : " + str(grad))
-		print("f_act : " + str(f_act) + "\nf_next : " + str(f_next))
-		print("f_next < f_act : ", not middle)
-		print ("direction : " + str(d) + "\n\n")
 		
+		
+		#print("f_next < f_act : ", not middle)
+		
+		print("\n\n")
+		
+	param = [k_list, a_list, lambda_list, grad_list, f_list]
+	return param
 	
 	
 
@@ -127,7 +152,8 @@ if __name__=="__main__":
 	#########################
 	
 	a = 2
-	b = 0.01
+	b = 0.1
+	n=20
 	
 	
 	#####################################################
@@ -136,7 +162,6 @@ if __name__=="__main__":
 	
 	init = (2,301)
 	nonoised = np.zeros(init)
-	
 	
 	xi=0
 	for i in range(len(nonoised[0])):
@@ -199,19 +224,62 @@ if __name__=="__main__":
 	#####################################################
 	#		Test de la methode de Levenberg-Marquardt	#
 	#####################################################	
-
-	lev_mar(0.001, 1.5, noised[0], noised[1])
-	#lev_mar(lambda, a,		x,		y	)
-
+	
+	
+	lm1 = lev_mar(0.001, 1.5, noised[0], noised[1], n, 0.0001)
+	#	  lev_mar(lambda, a , 		x  ,		y , n ,  grad_lim      )
+	
+	k_list = lm1[0]
+	a_list = lm1[1]
+	lambda_list = lm1[2]
+	grad_list = lm1[3]
+	f_list = lm1[4]
+	
+	a_opt = a_list[n-1]
+	
+	
+	plt.plot(k_list, a_list, 'r')
+	plt.xlabel('k')
+	plt.ylabel('a')
+	plt.title('Evolution du parametre a')
+	plt.show()
+	
+	plt.plot(k_list, lambda_list, 'g')
+	plt.xlabel('k')
+	plt.ylabel('lambda')
+	plt.title('Evolution du parametre lambda')
+	plt.show()
+	
+	plt.plot(k_list, grad_list, 'b')
+	plt.xlabel('k')
+	plt.ylabel('Norme du gradient')
+	plt.title('Evolution de la norme du gradient')
+	plt.show()
+	
+	plt.plot(k_list, f_list, 'm')
+	plt.xlabel('k')
+	plt.ylabel('f(a)')
+	plt.title('Evolution du parametre f(a)')
+	plt.show()
+	
+	
+	#Jeu de donnees pour le a optimal
+	g_aopt = []
+	i=0
+	for xi in nonoised[0]:
+		g_aopt.append(g(xi,a_opt))
+		i+=1
 	#print(noised)
 	
-	plt.plot(nonoised[0], nonoised[1], 'r', label = 'Courbe theorique de g')
+	#plt.plot(nonoised[0], nonoised[1], 'r', label = 'Courbe theorique de g avec a=2')
+	plt.plot(nonoised[0], g_aopt, 'r', label = 'Courbe optimisee de g')
 	plt.scatter(noised[0],noised[1], s=1, c='g', marker='o', label = 'Valeurs bruitees de g')
 	plt.legend(loc = 'upper right')
 	plt.xlabel('x')
 	plt.ylabel('y')
+	plt.show()
+
 	
-	#plt.show()
 #Modification inutile
 	
 	
